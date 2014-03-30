0b7c744b7b446ffc07aea0fe1c8bbbec9f61b62c
Sebastián de la Fuente <sebadlf@gmail.com>
2014-03-30 20:02:28 -0300

    Commit Inicial

---
+++ app/src/main/java/com/synergysolutions/myshow/app/Character.java
@@ -0,0 +1,27 @@
+package com.synergysolutions.myshow.app;
+
+/**
+ * Created by sebadlf on 30/03/14.
+ */
+public class Character {
+
+    private String name;
+
+    private String alias;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+}
